9dd44664a0749018fdb7deab84988e265381c12a
Elie Oko <elieoko100@gmail.com>
2023-05-13 02:35:34 +0100

    feat:module variable js

---
+++ js/basic/variable/script.js
@@ -0,0 +1,20 @@
+//Utilisation de let
+let maVariable = 12;
+
+let sizeChild = 1.84;
+
+let fullName = "Elie Oko";
+
+let status = false
+
+let genre = 'M'
+
+//Utilisation de const
+const tva = 0.16
+
+const key_secrete = "2jdh.duwidnddwoowefwiejfw.728bsn"
+
+const caractereValide = 12
+
+const reviewCode = true
+
