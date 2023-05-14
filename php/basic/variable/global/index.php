7a18249dd436471db681a4839a2cd248897631b1
Elie Oko <elieoko100@gmail.com>
2023-05-14 21:46:41 +0100

    build:features basic

---
+++ php/basic/variable/global/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+$_GET['nom'] = "Tychique";
+$_POST['key'] = "23@hdhhshs2839jjdwioxbf";
+$_SERVER['expo'] = 38;
+$_SESSION['expiration'] = "12/02/2024";
+
+?>
---
+++ php/basic/variable/primitive/index.php
@@ -0,0 +1,6 @@
+<?php
+$age = 12;
+$nom_etablissement = "ISC";
+$taxe = 0.16;
+$status = true;
+?>
