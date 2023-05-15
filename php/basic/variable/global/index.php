594640dedb8157dbc8e59cec8c1815a921305b81
Elie Oko <elieoko100@gmail.com>
2023-05-15 09:35:33 +0100

    feat:other features

--- php/basic/variable/global/index.php
+++ php/basic/variable/global/index.php
@@ -5,5 +5,7 @@ $_GET['nom'] = "Tychique";
 $_POST['key'] = "23@hdhhshs2839jjdwioxbf";
 $_SERVER['expo'] = 38;
 $_SESSION['expiration'] = "12/02/2024";
-
+$GLOBALS['my'] =20;
+$_REQUEST['user'] = "Elie Oko";
+print_r($_SERVER);
 ?>
--- php/basic/variable/primitive/index.php
+++ php/basic/variable/primitive/index.php
@@ -2,5 +2,6 @@
 $age = 12;
 $nom_etablissement = "ISC";
 $taxe = 0.16;
-$status = true;
+$status =  0b1010;
+echo $status;
 ?>
