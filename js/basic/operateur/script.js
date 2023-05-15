730a9fa81568f16001c11ba9d47ba5daf7d9214f
Elie Oko <elieoko100@gmail.com>
2023-05-15 09:36:15 +0100

    build:main fun

---
+++ js/basic/operateur/script.js
@@ -0,0 +1,72 @@
+// Affectation 
+    // Affectation simple =
+    let maVariable = "coding"// maVariable <- coding
+    // Affectation augmentée +=,-=,*=,/=,%=
+    let tour = 1;
+    tour += 2; //tour <- 3
+    tour *= 6; //tour <- 18
+    tour -= 4; //tour <- 14
+    tour /= 2; //tour <- 7
+    tour %= 7; //tour <- 0
+/* Arithmétique */
+    // Addition +
+    const somme = 2 + 4 //somme <- 6
+    // Soustraction -
+    const reste = somme - 2 // reste <- 4
+    // Multiplication *
+    const produit = somme * 3 // produit <- 18
+    // Division /
+    const quotient = produit / 2 // quotient <- 6
+    // Modulo %
+    const resteEcludient = produit / 2 // resteEcludient <- 0
+/* Logique */
+    // And &&
+
+    // Or || 
+
+    // Not !
+
+
+
+
+/* Comparaison */
+
+    // <
+
+    // >
+
+    // !=
+
+    // >=
+
+    // <=
+
+    // ==
+
+    // ===
+
+
+
+/* Regroupement */
+
+    //()
+
+    //string template ${}
+
+    // ``
+
+
+
+/* Unaire */
+    // ++ Incrementation
+    let compteur = 3
+    compteur++; 
+    // -- Decrementation
+    compteur-- // compteur <- 3
+
+/* Referencemment */
+    // .
+
+
+
+/* Spread */
---
+++ php/documentation.md
@@ -0,0 +1,6 @@
+## Ce module rassemble tout les bases qu'un dev php doit connaitre
+## Type de variable 
+    integer
+    float
+    string
+    bool
